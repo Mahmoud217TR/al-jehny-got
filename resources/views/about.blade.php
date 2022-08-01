0f95f0608d8f8fc77c3cecd6b37bc2573a6cf6b2
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-08-01 13:18:24 +0300

    update commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,10 +5,10 @@
 @section('content')
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-md-6">
+            <div class="col-md-6 text-center">
                 <h1 class="fw-bold mb-3">About {{ config('app.name') }}</h1>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dicta dolor laborum neque tempore explicabo omnis fuga eligendi exercitationem dolores nemo libero ex, molestias excepturi commodi! Inventore vel laboriosam aperiam excepturi deleniti quisquam labore asperiores voluptatem rem, enim quos laudantium optio dolore maiores. Officiis itaque expedita reprehenderit asperiores mollitia soluta nesciunt porro facilis maiores commodi ducimus, placeat qui, autem blanditiis! Quaerat inventore in vitae praesentium aut quisquam debitis fuga, aperiam rem facilis animi iste sequi labore laboriosam dignissimos? Tempore, unde? Quisquam, sequi repellendus! Ratione autem error a perferendis voluptatum distinctio pariatur quos cumque. Minima optio quidem nihil voluptates facere ipsa?
+                    We provide you with the most valuable products with the highest quality at a fair price. <br>
                 </p>
             </div>
             <div class="col-md-4 offset-md-2">
