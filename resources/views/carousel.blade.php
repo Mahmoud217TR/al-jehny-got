adccc98346ae4badffc174f2236fe51106ae32e7
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-08-01 13:13:31 +0300

    updated

--- resources/views/carousel.blade.php
+++ resources/views/carousel.blade.php
@@ -1,13 +1,19 @@
 <div id="carouselControls" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner main-carousel">
       <div class="carousel-item active">
-        <img src="{{ asset('images/Gallery/1.png') }}" class="d-block w-100" alt="gallery picture">
+        <img src="{{ asset('images/Gallery/1.jpg') }}" class="d-block w-100" alt="gallery picture">
       </div>
       <div class="carousel-item">
-        <img src="{{ asset('images/Gallery/2.png') }}" class="d-block w-100" alt="gallery picture">
+        <img src="{{ asset('images/Gallery/2.jpg') }}" class="d-block w-100" alt="gallery picture">
       </div>
       <div class="carousel-item">
-        <img src="{{ asset('images/Gallery/3.png') }}" class="d-block w-100" alt="gallery picture">
+        <img src="{{ asset('images/Gallery/3.jpg') }}" class="d-block w-100" alt="gallery picture">
+      </div>
+      <div class="carousel-item">
+        <img src="{{ asset('images/Gallery/4.jpg') }}" class="d-block w-100" alt="gallery picture">
+      </div>
+      <div class="carousel-item">
+        <img src="{{ asset('images/Gallery/5.jpg') }}" class="d-block w-100" alt="gallery picture">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
